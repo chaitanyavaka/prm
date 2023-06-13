f29fe3a3f7dcebe680f6cc50e97e797468cb4820
chaitanyavaka <chaitanyavaka82@gmail.com>
2023-06-13 11:19:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,205 @@
+from flask import Flask,redirect,url_for,render_template,request,flash,abort,session
+from flask_session import Session
+from key import secret_key,salt1,salt2
+from stoken import token
+from cmail import sendmail
+from itsdangerous import URLSafeTimedSerializer
+import mysql.connector
+app=Flask(__name__)
+app.secret_key=secret_key
+app.config['SESSION_TYPE']='filesystem'
+Session(app)
+mydb=mysql.connector.connect(host='localhost',user='root',password='admin',db='prm')
+@app.route('/')
+def index():
+    return render_template('title.html')
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if session.get('user'):
+        return redirect(url_for('home'))
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        cursor=mydb.cursor(buffered=True)
+        cursor.execute('select count(*) from users where username=%s',[username])
+        count=cursor.fetchone()[0]
+        if count==1:
+            cursor.execute('select count(*) from users where username=%s and password=%s',[username,password])
+            p_count=cursor.fetchone()[0]
+            if p_count==1:
+                session['user']=username
+                cursor.execute('select email_status from users where username=%s',[username])
+                status=cursor.fetchone()[0]
+                cursor.close()
+                if status!='confirmed':
+                    return redirect(url_for('inactive'))
+                else:
+                    return redirect(url_for('home'))
+            else:
+                cursor.close()
+                flash('invalid password')
+                return render_template('login.html')
+        else:
+            cursor.close()
+            flash('invalid username')
+            return render_template('login.html')
+    return render_template('login.html')
+@app.route('/inactive')
+def inactive():
+    if session.get('user'):
+        username=session.get('user')
+        cursor=mydb.cursor(buffered=True)
+        cursor.execute('select email_status from users where username=%s',[username])
+        status=cursor.fetchone()[0]
+        cursor.close()
+        if status=='confirmed':
+            return redirect(url_for('home'))
+        else:
+            return render_template('inactive.html')
+    else:
+        return redirect(url_for('login'))
+@app.route('/homepage')
+def home():
+    if session.get('user'):
+        username=session.get('user')
+        cursor=mydb.cursor(buffered=True)
+        cursor.execute('select email_status from users where username=%s',[username])
+        status=cursor.fetchone()[0]
+        cursor.close()
+        if status=='confirmed':
+            return render_template('homepage.html')
+        else:
+            return redirect(url_for('inactive'))
+    else:
+        return redirect(url_for('login'))
+@app.route('/resendconfirmation')
+def resend():
+    if session.get('user'):
+        username=session.get('user')
+        cursor=mydb.cursor(buffered=True)
+        cursor.execute('select email_status from users where username=%s',[username])
+        status=cursor.fetchone()[0]
+        cursor.execute('select email from users where username=%s',[username])
+        email=cursor.fetchone()[0]
+        cursor.close()
+        if status=='confirmed':
+            flash('Email already confirmed')
+            return redirect(url_for('home'))
+        else:
+            subject='Email Confirmation'
+            confirm_link=url_for('confirm',token=token(email,salt1),_external=True)
+            body=f"Please confirm your mail-\n\n{confirm_link}"
+            sendmail(to=email,body=body,subject=subject)
+            flash('Confirmation link sent check your email')
+            return redirect(url_for('inactive'))
+    else:
+        return redirect(url_for('login'))
+@app.route('/registration',methods=['GET','POST'])
+def registration():
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        email=request.form['email']
+        cursor=mydb.cursor(buffered=True)
+        try:
+            cursor.execute('insert into users (username,password,email) values(%s,%s,%s)',(username,password,email))
+        except mysql.connector.IntegrityError:
+            flash('Username or email is already in use')
+            return render_template('registration.html')
+        else:
+            mydb.commit()
+            cursor.close()
+            subject='Email Confirmation'
+            confirm_link=url_for('confirm',token=token(email,salt1),_external=True)
+            body=f"Thanks for signing up.Follow this link-\n\n{confirm_link}"
+            sendmail(to=email,body=body,subject=subject)
+            flash('Confirmation link sent check your email')
+            return render_template('registration.html')
+    return render_template('registration.html')
+    
+@app.route('/confirm/<token>')
+def confirm(token):
+    try:
+        serializer=URLSafeTimedSerializer(secret_key)
+        email=serializer.loads(token,salt=salt1,max_age=120)
+    except Exception as e:
+        #print(e)
+        abort(404,'Link expired')
+    else:
+        cursor=mydb.cursor(buffered=True)
+        cursor.execute('select email_status from users where email=%s',[email])
+        status=cursor.fetchone()[0]
+        cursor.close()
+        if status=='confirmed':
+            flash('Email already confirmed')
+            return redirect(url_for('login'))
+        else:
+            cursor=mydb.cursor(buffered=True)
+            cursor.execute("update users set email_status='confirmed' where email=%s",[email])
+            mydb.commit()
+            flash('Email confirmation success')
+            return redirect(url_for('login'))
+@app.route('/forget',methods=['GET','POST'])
+def forgot():
+    if request.method=='POST':
+        email=request.form['email']
+        cursor=mydb.cursor(buffered=True)
+        cursor.execute('select count(*) from users where email=%s',[email])
+        count=cursor.fetchone()[0]
+        cursor.close()
+        if count==1:
+            cursor=mydb.cursor(buffered=True)
+            cursor.execute('SELECT email_status from users where email=%s',[email])
+            status=cursor.fetchone()[0]
+            cursor.close()
+            if status!='confirmed':
+                flash('Please Confirm your email first')
+                return render_template('forgot.html')
+            else:
+                subject='Forget Password'
+                confirm_link=url_for('reset',token=token(email,salt=salt2),_external=True)
+                body=f"Use this link to reset your password-\n\n{confirm_link}"
+                sendmail(to=email,body=body,subject=subject)
+                flash('Reset link sent check your email')
+                return redirect(url_for('login'))
+        else:
+            flash('Invalid email id')
+            return render_template('forgot.html')
+    return render_template('forgot.html')
+@app.route('/reset/<token>',methods=['GET','POST'])
+def reset(token):
+    try:
+        serializer=URLSafeTimedSerializer(secret_key)
+        email=serializer.loads(token,salt=salt2,max_age=180)
+    except:
+        abort(404,'Link Expired')
+    else:
+        if request.method=='POST':
+            newpassword=request.form['npassword']
+            confirmpassword=request.form['cpassword']
+            if newpassword==confirmpassword:
+                cursor=mydb.cursor(buffered=True)
+                cursor.execute('update users set password=%s where email=%s',[newpassword,email])
+                mydb.commit()
+                flash('Reset Successful')
+                return redirect(url_for('login'))
+            else:
+                flash('Passwords mismatched')
+                return render_template('newpassword.html')
+        return render_template('newpassword.html')
+
+@app.route('/logout')
+def logout():
+    if session.get('user'):
+        session.pop('user')
+        return redirect(url_for('login'))
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/addnotes',methods=['POST','GET'])
+def addnotes():
+    return render_template('addnotes.html')
+@app.route('/viewnotes')
+def viewnotes():
+    return render_template('table.html')
+app.run(debug=True,use_reloader=True)
